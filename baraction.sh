19eee42c9dc154d8fa0615232ae37aff4eb879d8
Roman Ivanov <varejy@gmail.com>
2020-06-19 19:03:55 +0300

    Init

--- baraction.sh
+++ baraction.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+function cpu() {
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep .5 
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -n "+@fn=1;+@fn=0;+1<$cpu%+@fn=1;1<"
+}
+function using_mem() {
+	mem=$(free | awk '$1=="Mem:" {result = $3 / $2 * 100; printf "%.0f", result}')
+	echo -n "+@fn=1;+@fn=0;+1<$mem%+@fn=1;1<"
+}
 function battery() {
 	capacity=$(< /sys/class/power_supply/BAT0/capacity)
 	if (($capacity >= 0 && $capacity <= 5))
@@ -23,9 +36,11 @@ function vol() {
 }
 
 while :; do
+	cpu
+	using_mem	
 	vol
 	battery
 	echo "+@fn=0;"
 	
-	sleep 1
+	sleep .5
 done
