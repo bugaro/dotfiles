62de4de6eb20ae092b24eb07317fcc0106e7cbbb
Roman Ivanov <varejy@gmail.com>
2020-06-29 22:34:04 +0300

    Init

--- baraction.sh
+++ baraction.sh
@@ -10,11 +10,13 @@ function cpu() {
   read cpu a b c idle rest < /proc/stat
   total=$((a+b+c+idle)) 
   cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) )) 
-  echo -n "+@fn=1;+@fn=0;+1<$cpu%+@fn=1;1<"
+  echo -n "+@fn=1;+@fn=0;+1<$cpu%"
+  echo -n "+@fn=0;+3<"
 }
 function using_mem() {
 	mem=$(free | awk '$1=="Mem:" {result = $3 / $2 * 100; printf "%.0f", result}')
-	echo -n "+@fn=1;+@fn=0;+1<$mem%+@fn=1;1<"
+	echo -n "+@fn=1;+@fn=0;+1<$mem%"
+	echo -n "+@fn=0;+3<"
 }
 function net_traffic() {
 	IFS=' ' read -r -a data <<< "$(cat $HOME/.vnstat.log)"
@@ -24,30 +26,33 @@ function net_traffic() {
 	download=${data[0]}
 	upload=${data[1]}
 	fi
-	echo -n "+@fn=1;+@fn=0;+1<$download+@fn=1;<+@fn=0;+1<$upload+@fn=1;1<" 
+	echo -n "+@fn=1;+@fn=0;+1<$download+3<+@fn=1;+@fn=0;+1<$upload" 
+	echo -n "+@fn=0;+3<"
 }
 function battery() {
 	capacity=$(< /sys/class/power_supply/BAT0/capacity)
 
 	if (($capacity >= 0 && $capacity <= 5))
-	then echo -n "+@fn=1;1<"
+	then echo -n "+@fn=1;"
 	elif (($capacity > 5 && $capacity <= 25))
-	then echo -n "+@fn=1;1<"
+	then echo -n "+@fn=1;"
 	elif (($capacity > 25 && $capacity <= 50))
-	then echo -n "+@fn=1;1<"
+	then echo -n "+@fn=1;"
 	elif (($capacity > 50 && $capacity <= 75))
-	then echo -n "+@fn=1;1<"
-	else echo -n "+@fn=1;1<"
+	then echo -n "+@fn=1;"
+	else echo -n "+@fn=1;"
 	fi
+	echo -n "+@fn=0;+3<"
 } 
 function vol() { 
 	vol=$(pactl list sinks | awk '$1=="Volume:" {print $5}') 
 	mute=$(pactl list sinks | awk '$1=="Mute:" {print $2}') 
 
 	if [[ $mute == "no" ]]; then 
-	echo -n "+@fn=1;+@fn=0;+1<$vol+@fn=1;1<"
-	else echo -n "+@fn=1;1<"
+	echo -n "+@fn=1;+@fn=0;+1<$vol"
+	else echo -n "+@fn=1;"
 	fi 
+	echo -n "+@fn=0;+3<"
 } 
 	while :; do 
 	cpu 
